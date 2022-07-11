0731df68a1003b228d48b3915035ec56e0b61470
sabiinsearch <sabiinsearch@gmail.com>
2022-07-11 15:20:48 +0530

    Adding Energy_Monitoring_lib

---
+++ Code/ESP/WT_Energy_Monitoring_v2/lib/myCust_lib/Energy_Monitoring.h
@@ -0,0 +1,9 @@
+#ifndef __ENERGYMONITORING_H__
+#define __ENERGYMONITORING_H__
+
+#include "Arduino.h"
+
+void ernergy_consumption(void * pvParameters)
+
+
+#endif
--- Code/ESP/WT_Energy_Monitoring_v2/lib/myCust_lib/myCommon.h
+++ Code/ESP/WT_Energy_Monitoring_v2/lib/myCust_lib/myCommon.h
@@ -1,8 +1,10 @@
 #ifndef __MYCOMMON_H__
 #define __MYCOMMON_H__
 
-#include "Communication.h"
+
 #include "receiverBoard.h"
+#include "Communication.h"
+#include "Energy_Monitoring.h"
 
   void getMyMsg();
   
